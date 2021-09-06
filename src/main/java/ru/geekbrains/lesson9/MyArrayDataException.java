91c3f033cf7efc640daae603ea822077e7e6e121
megapxdll <mkuanyshbek97@mail.ru>
2021-09-06 15:07:39 +0600

    1

---
+++ src/main/java/ru/geekbrains/lesson9/MyArrayDataException.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.lesson9;
+
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
